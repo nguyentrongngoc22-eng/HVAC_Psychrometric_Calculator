53d3fe51b545f492ab4cd83e2aedd5196e72b471
nguyentrongngoc22-eng <nguyentrongngoc22@gmail.com>
2026-07-12 22:10:35 +0700

    Add files via upload

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = "hvac-psychrometric-v1.0.1";
-const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match("./index.html"))))});
